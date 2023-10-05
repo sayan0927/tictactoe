e3cd54d383cf66a59c2eba30c0369de404b8da05
sayan0927 <90630147+sayan0927@users.noreply.github.com>
2023-10-05 21:17:18 +0530

    Update HumanPlayer.java

--- src/GAME/HumanPlayer.java
+++ src/GAME/HumanPlayer.java
@@ -27,32 +27,8 @@ public class HumanPlayer extends Player {
 
     @Override
     public MoveDetails newMove(GameBoard gameBoard) {
-
-
         return gameWindow.getLastMove();
-
-      /* while (true) {
-           Semaphore semaphore = new Semaphore(0);
-           gameWindow.setCurrentSymbol(this.getSymbol());
-           System.out.println(this.getSymbol());
-           gameWindow.setSemaphore(semaphore);
-           try {
-               semaphore.acquire();
-           } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-           MoveDetails last = gameWindow.getLastMove();
-           if(gameBoard.isValidInput(last))
-               return last;
-           else
-               gameWindow.showInvalidMoveError();
-       }
-
-       */
-
     }
-    public void setLock(Semaphore lock) {
-
-    }
+   
 
 }
